979bb969c11373742247345b400adabde2044903
divinestylus <divinestylus@gmail.com>
2023-05-20 08:02:01 +0000

    May 20, 2023 updates | 8:05 AM

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
+// Getting page items
 let menu = document.querySelector('.menu');
 let navBar = document.querySelector(".hero-section nav div");
 
+// Mobile menu display toggle
 menu.addEventListener('click', function(){
     navBar.classList.toggle('showMenu');
 })
