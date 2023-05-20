8526943169d144f17ea6f23b7300251d19ed6ab5
divinestylus <divinestylus@gmail.com>
2023-05-20 10:07:00 +0000

    Hamburger to cancel icon toggle

--- scripts/app.js
+++ scripts/app.js
@@ -3,8 +3,10 @@ let menu = document.querySelector('.menu');
 let navBar = document.querySelector(".hero-section nav div");
 
 // Mobile menu display toggle
+// Hamburger to cancel toggle
 menu.addEventListener('click', function(){
     navBar.classList.toggle('showMenu');
+    menu.classList.toggle('fa-times');
 })
 
 
